57161b712aeabba339deaf73e91b11ef137bebca
Kharadin <99069894+Kharadin@users.noreply.github.com>
2026-02-01 13:53:14 +0300

    Update app/ConvexClientProvider.jsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/ConvexClientProvider.jsx
+++ app/ConvexClientProvider.jsx
@@ -1,7 +1,6 @@
 "use client";
 
-// import { useAuth } from "@clerk/nextjs";
-import { useAuth } from "@clerk/clerk-react";
+import { useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import {ConvexProviderWithClerk} from "convex/react-clerk"
  
